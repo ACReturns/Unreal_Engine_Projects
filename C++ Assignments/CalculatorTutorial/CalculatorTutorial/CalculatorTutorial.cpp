619768222e9a72c3b3e3cdfa88e0799afd8ec9ec
ACReturns <DSXtreme2012@gmail.com>
2024-07-15 21:49:44 -0700

    Wrap up
    - Pushing tail end of school stuff that wasn't backed up

--- C++ Assignments/CalculatorTutorial/CalculatorTutorial/CalculatorTutorial.cpp
+++ C++ Assignments/CalculatorTutorial/CalculatorTutorial/CalculatorTutorial.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include "Calculator.h"
+#include <string>
+#include <stdio.h>
 
 using namespace std;
 
@@ -12,6 +14,7 @@ int main()
     double y = 0.0;
     double result = 0.0;
     char oper = '+';
+    
 
     cout << "Calculator Console Application" << endl << endl;
     cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
@@ -21,17 +24,30 @@ int main()
     while (true)
     {
         cin >> x >> oper >> y;
+        // Sort out how to verify that invalid inputs also exit out ie: _90 should return saying invalid number
+        
         if (oper == '/' && y == 0)
         {
             cout << "Division by 0 exception" << endl;
             continue;
         }
+        else if (cin.fail())
+        {
+            cout << "Not a valid number" << endl;
+            break;
+        }
         else
         {
 			result = c.Calculate(x, oper, y);
+            cout << "Result is: " << result << endl;
         }
-        cout << "Result is: " << result << endl;
     }
 
     return 0;
+}
+
+void userQuestion()
+{
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
+        << endl;
 }
